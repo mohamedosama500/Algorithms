eda7b13f626a7f8cf24bb2f02c9582e0b2c0b20e
mohamed osama <muhammed.osama012@gmail.com>
2026-05-27 23:55:10 +0300

    Modifying the comments in Binary_tree_diameter.cpp file

--- Binary_tree_diameter.cpp
+++ Binary_tree_diameter.cpp
@@ -10,12 +10,26 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// Function to calculate the height of the tree
+
+/*
+Function to calculate the height of the tree.
+The recursion goes all the way down to the leaf nodes.
+Their children are NULL, so they return a height of 0 for this childern.
+The & means diameter is passed by reference. Any changes made to it inside the recursive calls
+Are saved globally across the entire tree exploration.
+*/
 int height(TreeNode* root, int& diameter) {
     if (root == NULL)
         return 0;
 
-    // Recursive calls to calculate the height of left and right subtrees
+    /* Recursive calls to calculate the height of the left and right subtrees.
+       Traverses down to the leaf nodes, then bubbles back up to the parent nodes 
+       as the recursive calls resolve.
+       1. Root starts ───> Paused at leftHeight
+       2.   Left Child ───> Paused at leftHeight
+       3.     Leaf Node ───> Paused at leftHeight
+       4.       NULL reached! Returns 0.
+    */
     int leftHeight = height(root->left, diameter);
     int rightHeight = height(root->right, diameter);
 
@@ -23,6 +37,8 @@ int height(TreeNode* root, int& diameter) {
     diameter = max(diameter, leftHeight + rightHeight);
 
     // Return the height of the tree rooted at the current node
+    // Once the leaf node finishes its last line (e.g. return max(0,0) + 1), it dies
+    // and returns 1 to its parent.
     return max(leftHeight, rightHeight) + 1;
 }
 
